61c70c5dd30b21bc2c3aade5d2e9080604892700
WYS <silence14@hanmail.net>
2022-01-24 00:56:11 +0900

    Comment out outpus

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "mysql_instance_id" {
   value = aws_db_instance.mysql-db.id
 }
@@ -9,3 +10,4 @@ output "mysql_endpoint" {
 output "redis_instance_id" {
  value = aws_elasticache_cluster.ms_redis
 }
+*/
